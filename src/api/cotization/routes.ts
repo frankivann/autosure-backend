ee531127b8a434eae1780f92c76f4873ce2c6307
Franklin Alvarado <frankivan.dev@gmail.com>
2024-03-23 09:12:39 -0500

    remove pre cotization route

--- src/api/cotization/routes.ts
+++ src/api/cotization/routes.ts
@@ -1,12 +1,11 @@
 import { Router } from 'express'
-import { addCotization, getCotizations, getUserCotizations, preCotization } from './controller'
+import { addCotization, getCotizations, getUserCotizations } from './controller'
 import asyncHandler from 'express-async-handler'
 const router = Router()
 
 router
   .get('/', asyncHandler(getCotizations))
   .get('/user', asyncHandler(getUserCotizations))
-  .get('/pre', asyncHandler(preCotization))
   .post('/', asyncHandler(addCotization))
 
 export default router
