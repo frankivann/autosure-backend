b2cc00efa4a51cab5eeac06f3a2051e5329c09c5
Franklin Alvarado <frankivan.dev@gmail.com>
2024-03-22 20:28:03 -0500

    fix cotization endpoints

--- src/api/cotization/routes.ts
+++ src/api/cotization/routes.ts
@@ -1,13 +1,12 @@
 import { Router } from 'express'
 import { addCotization, getCotizations, getUserCotizations, preCotization } from './controller'
-import { authorization } from '@middlewares/authorization'
 import asyncHandler from 'express-async-handler'
 const router = Router()
 
 router
-  .get('/', authorization, asyncHandler(getCotizations))
+  .get('/', asyncHandler(getCotizations))
+  .get('/user', asyncHandler(getUserCotizations))
   .get('/pre', asyncHandler(preCotization))
-  .get('/record', authorization, asyncHandler(getUserCotizations))
-  .post('/', authorization, asyncHandler(addCotization))
+  .post('/', asyncHandler(addCotization))
 
 export default router
