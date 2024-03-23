19c4ba1655a1d8c9cf2c5792a8b07933a959318d
Franklin Alvarado <frankivan.dev@gmail.com>
2024-03-22 20:27:34 -0500

    refactor cotization controller

--- src/api/cotization/controller.ts
+++ src/api/cotization/controller.ts
@@ -1,21 +1,18 @@
 import { Request, Response, NextFunction } from 'express'
-import { USER_ROLES } from '@src/constants'
-import CotizationModel from './model'
+// import { USER_ROLES } from '@src/constants'
 import UserModel from '@api/user/model'
+import CotizationModel from './model'
 
 export async function getCotizations (req: Request, res: Response, next: NextFunction): Promise<void> {
-  const { userRole } = req
+  // const { userRole } = req
 
   try {
-    if (userRole !== USER_ROLES.ADMIN) {
-      res.status(401).json({ error: true, message: 'Permitido solo a administración' })
-      return
-    }
+    // if (userRole !== USER_ROLES.ADMIN) {
+    //   res.status(401).json({ error: true, message: 'Permitido solo a administración' })
+    //   return
+    // }
 
-    const cotizations = await CotizationModel.find({}).populate('userId', {
-      firstname: 1,
-      lastname: 1
-    })
+    const cotizations = await CotizationModel.find({})
     res.json({ cotizations })
   } catch (error) {
     next(error)
@@ -45,31 +42,19 @@ export async function getUserCotizations (req: Request, res: Response, next: Nex
 
 export async function preCotization (_: Request, res: Response, next: NextFunction): Promise<void> {
   try {
-    /**
-     * Check if there are cotizations
-     * if not return 1
-     */
-
-    const cotizations = await CotizationModel.find({})
-    if (cotizations.length === 0) {
-      res.json({ currentCotization: 1 })
-      return
-    }
-
-    const cotizationNumbers = cotizations.map(cotization => cotization.cotizationNumber)
-    res.json({ currentCotization: (cotizationNumbers.at(-1) as number) + 1 })
+    res.json({ m: 'm' })
   } catch (error) {
     next(error)
   }
 }
 
 export async function addCotization (req: Request, res: Response, next: NextFunction): Promise<void> {
-  const { body, userId } = req
+  // const { body, userId } = req
 
   try {
-    const newCotization = new CotizationModel({ ...body, userId })
+    // const newCotization = new CotizationModel({ ...body, userId })
 
-    await newCotization.save()
+    // await newCotization.save()
     res.status(201).json({ message: 'Cotización agregada correctamente' })
   } catch (error) {
     next(error)
