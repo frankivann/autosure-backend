cf21bd7c547885977372401bd2cd87a5ec3d0c93
Franklin Alvarado <frankivan.dev@gmail.com>
2024-03-22 21:02:03 -0500

    improving cotization schema

--- src/api/cotization/model.ts
+++ src/api/cotization/model.ts
@@ -1,53 +1,71 @@
 import { Schema, model } from 'mongoose'
-import { type CarInsurance } from '@src/types/global'
-import { CAR_BRANDS, FUEL_TYPES, LOCATIONS, TRANSMISSION_TYPES, USAGES } from '@src/constants'
+import { type Cotization } from '@src/types/global'
+import { CAR_BRANDS, CAR_MODELS, FUEL_TYPES, LOCATIONS, TRANSMISSION_TYPES, USAGES } from '@src/constants'
 
-const CotizationSchema = new Schema<CarInsurance>({
+const CotizationSchema = new Schema<Cotization>({
   year: {
     type: Number,
     required: true,
+    lowercase: true,
     trim: true
   },
   brand: {
     type: String,
     enum: CAR_BRANDS,
     required: true,
+    lowercase: true,
     trim: true
   },
   model: {
     type: String,
     required: true,
+    enum: CAR_MODELS,
+    lowercase: true,
     trim: true
   },
   fuelType: {
     type: String,
     enum: FUEL_TYPES,
     required: true,
+    lowercase: true,
     trim: true
   },
   transmissionType: {
     type: String,
     enum: TRANSMISSION_TYPES,
     required: true,
+    lowercase: true,
     trim: true
   },
   numberOfDoors: {
     type: Number,
     enum: [2, 4],
     required: true,
+    lowercase: true,
     trim: true
   },
   location: {
     type: String,
     enum: LOCATIONS,
     required: true,
+    lowercase: true,
     trim: true
   },
   usage: {
     type: String,
     enum: USAGES,
     required: true,
+    lowercase: true,
     trim: true
+  },
+  price: {
+    type: Number,
+    required: true
+  },
+  userId: {
+    type: Schema.Types.ObjectId,
+    ref: 'User',
+    required: true
   }
 })
 
