3430f8a2ee2280333f32645c9fbecaebc4f4510b
Franklin Alvarado <frankivan.dev@gmail.com>
2024-03-22 21:01:35 -0500

    improving app constants

--- src/constants.ts
+++ src/constants.ts
@@ -3,8 +3,21 @@ export const USER_ROLES = {
   ADMIN: 'admin'
 } as const
 
-export const CAR_BRANDS = ['Toyota', 'Honda', 'Ford', 'Chevrolet', 'Volkswagen', 'Nissan', 'BMW', 'Mercedes-Benz', 'Audi', 'Hyundai'] as const
 export const FUEL_TYPES = ['Gasoline', 'Diesel', 'Hybrid', 'Electric'] as const
 export const TRANSMISSION_TYPES = ['Automatic', 'Manual'] as const
 export const LOCATIONS = ['City', 'Rural'] as const
 export const USAGES = ['Personal', 'Commercial'] as const
+
+export const CARS = {
+  Toyota: ['Corolla', 'Camry', 'RAV4', 'Prius'],
+  Honda: ['Civic', 'Accord', 'CR-V', 'HR-V'],
+  Ford: ['Fiesta', 'Focus', 'Fusion', 'Escape'],
+  Chevrolet: ['Spark', 'Cruze', 'Malibu', 'Equinox'],
+  Nissan: ['Sentra', 'Altima', 'Rogue', 'Murano'],
+  BMW: ['Serie 3', 'Serie 5', 'X3', 'X5'],
+  Audi: ['A3', 'A4', 'Q3', 'Q5'],
+  Hyundai: ['Elantra', 'Sonata', 'Tucson', 'Santa Fe']
+} as const
+
+export const CAR_BRANDS = Object.keys(CARS)
+export const CAR_MODELS = Object.values(CARS).flat()
