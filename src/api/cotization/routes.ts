49a96665d1f3f2ca0573191e47e8ceaa60b78510
Franklin Alvarado <frankivan.dev@gmail.com>
2024-03-24 20:06:56 -0500

    add authorization middleware

--- src/api/cotization/routes.ts
+++ src/api/cotization/routes.ts
@@ -1,11 +1,12 @@
 import { Router } from 'express'
 import { addCotization, getCotizations, getUserCotizations } from './controller'
+import { authorization } from '@middlewares/authorization'
 import asyncHandler from 'express-async-handler'
 const router = Router()
 
 router
-  .get('/', asyncHandler(getCotizations))
-  .get('/user', asyncHandler(getUserCotizations))
-  .post('/', asyncHandler(addCotization))
+  .get('/', authorization, asyncHandler(getCotizations))
+  .get('/user', authorization, asyncHandler(getUserCotizations))
+  .post('/', authorization, asyncHandler(addCotization))
 
 export default router
