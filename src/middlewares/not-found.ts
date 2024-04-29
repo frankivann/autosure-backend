7410f6d41237365f4fb9adc73a9a791a4025c773
frankivann <frankivan.dev@gmail.com>
2024-04-29 15:32:35 -0500

    improve response message

--- src/middlewares/not-found.ts
+++ src/middlewares/not-found.ts
@@ -1,5 +1,8 @@
 import { Request, Response } from 'express'
 
 export default function (_req: Request, res: Response): void {
-  res.sendStatus(404)
+  res.status(404).json({
+    error: true,
+    message: 'Not found'
+  })
 }
